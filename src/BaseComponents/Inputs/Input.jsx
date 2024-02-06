9084a7ca3c2f717dd8e0ad1f88fbdc4c3efcfe8c
pawarts <98350667+pawarts@users.noreply.github.com>
2024-02-06 12:41:04 +0200

    Add files via upload

--- src/BaseComponents/Inputs/Input.jsx
+++ src/BaseComponents/Inputs/Input.jsx
@@ -20,7 +20,7 @@ const Input = (props) => {
         <div className={s.input_wrapper}>
             <input id={`${input_name}`} className={`${s.enter_input} ${isNotEmpty ? s.active_input : ''}`}
                 onInput={event => inputValueLengthChecker(event)}
-                type="text" name={`${input_name}`} value={props.value}
+                type={props.type || "text"} name={`${input_name}`} value={props.value}
                 onChange={(event) => props.changeInput(event, input_name)} />
             <label htmlFor={`${input_name}`}
                 className={`${s.input_title}
