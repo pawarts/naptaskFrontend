db2af40f931c0454970187d68c31a157c99896e5
pawarts <98350667+pawarts@users.noreply.github.com>
2024-03-04 15:35:29 +0200

    Add files via upload

--- src/Schedule/Component/ScheduleInfo/ScheduleInfo.jsx
+++ src/Schedule/Component/ScheduleInfo/ScheduleInfo.jsx
@@ -25,18 +25,23 @@ const ScheduleInfo = (props) => {
 
     return (
         <div className={s.wrapper}>
-            <ScheduleHeader openEditWindow={openEditWindow} />
+            <div style={{
+                overflowY: createSchedule ? 'hidden' : 'scroll',
+                maxHeight: createSchedule ? '100vh' : 'auto'
+            }}>
+                <ScheduleHeader openEditWindow={openEditWindow} />
 
-            <h1 className="screen_title">{schedule.title}</h1>
+                <h1 className="screen_title">{schedule.title}</h1>
 
-            <div>
-                <ScheduleInfoItem info_item={schedule.scheduleBody.mon} day_number={0} />
-                <ScheduleInfoItem info_item={schedule.scheduleBody.tue} day_number={1} />
-                <ScheduleInfoItem info_item={schedule.scheduleBody.wed} day_number={2} />
-                <ScheduleInfoItem info_item={schedule.scheduleBody.thu} day_number={3} />
-                <ScheduleInfoItem info_item={schedule.scheduleBody.fri} day_number={4} />
-                <ScheduleInfoItem info_item={schedule.scheduleBody.sat} day_number={5} />
-                <ScheduleInfoItem info_item={schedule.scheduleBody.sun} day_number={6} />
+                <div>
+                    <ScheduleInfoItem info_item={schedule.scheduleBody.mon} day_number={0} />
+                    <ScheduleInfoItem info_item={schedule.scheduleBody.tue} day_number={1} />
+                    <ScheduleInfoItem info_item={schedule.scheduleBody.wed} day_number={2} />
+                    <ScheduleInfoItem info_item={schedule.scheduleBody.thu} day_number={3} />
+                    <ScheduleInfoItem info_item={schedule.scheduleBody.fri} day_number={4} />
+                    <ScheduleInfoItem info_item={schedule.scheduleBody.sat} day_number={5} />
+                    <ScheduleInfoItem info_item={schedule.scheduleBody.sun} day_number={6} />
+                </div>
             </div>
 
             <div>
