345af6d465d537b2d9e3f8c93964792ad4d90d0b
pawarts <98350667+pawarts@users.noreply.github.com>
2024-05-15 12:30:00 +0300

    Delete BaseComponents directory

--- BaseComponents/Buttons/SmallSubmitButton.jsx
+++
@@ -1,9 +0,0 @@
-import s from './ComponentStyles/Button.module.css'
-
-const SmallSubmitButton = ({text, action}) => {
-    return (
-        <button className={`black ${s.small_submit_button}`} onClick={action}>{text}</button>
-    )
-}
-
-export default SmallSubmitButton
